f7cf42f64dd6bfae3c9f184d9e553c1b3627b416
Burak Uzunboy <burak@beatrips.com>
2017-08-02 20:47:08 +0300

    Created repo

---
+++ beatrips/EventsView.swift
@@ -0,0 +1,33 @@
+//
+//  EventsView.swift
+//  beatrips
+//
+//  Created by Burak Uzunboy on 30.07.2017.
+//  Copyright © 2017 Burak Uzunboy. All rights reserved.
+//
+
+import UIKit
+
+class EventsView: UIView {
+
+    @IBOutlet weak var eventName: UILabel!
+    @IBOutlet weak var eventDate: UILabel!
+    @IBOutlet weak var venueName: UILabel!
+    @IBOutlet weak var venueImage: UIImageView!
+    @IBOutlet weak var eventImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+   
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- beatrips/ViewControllers/HomeViewController.swift
+++ beatrips/ViewControllers/HomeViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, iCarouselDataSource, iCarouselDelegate {
     
+    @IBOutlet var carousel: iCarousel!
+
+    var items = [1,2,3,4,6]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        carousel.type = .linear
+        
 
         // Do any additional setup after loading the view.
     }
@@ -31,6 +36,32 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         
         return cell
+    }
+    
+    func numberOfItems(in carousel: iCarousel) -> Int {
+        return items.count
+    }
+    
+    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+
+        var itemView = UINib(nibName: "EventsView", bundle: nil).instantiate(withOwner: nil, options: nil).first as! EventsView
+        itemView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width * 0.9, height: 240)
+        
+        itemView.eventName.text = String(index)
+        itemView.eventDate.text = "10 Temmuz"
+        return itemView
+
+        //reuse view if available, otherwise create a new view
+      
+        //return itemView
+    }
+    
+    func carousel(_ carousel: iCarousel, valueFor option: iCarouselOption, withDefault value: CGFloat) -> CGFloat {
+      
+        if (option == .wrap) {
+            return 1
+        }
+        return value
     }
 
     /*
